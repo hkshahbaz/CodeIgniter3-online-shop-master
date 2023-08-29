610208f275833e8ab3e7f280b8497f46292c1957
Salman Bhatti <142496543+mtaiyabaziz@users.noreply.github.com>
2023-08-29 21:20:21 +0500

    Tuesday
    --Commits

---
+++ application/modules/templates/views/_user_not_connected.php
@@ -0,0 +1,63 @@
+ <div class="header-middle">
+        <div class="container header-middle-cont">
+            <div class="toplogo">
+                <a href="<?php echo base_url(); ?>">
+                    <img src="<?php echo base_url();?>technipack_pictures/technipacklogo.png" alt="Technipack - Technique de packaging" style="margin-left: 500px;">
+                </a>
+            </div>
+            <div class="shop-menu">
+                <ul>
+
+                   <br>  <br>  <br>
+
+                    <li class="topauth" style="margin-left: 150px;">
+                        <a href="<?= base_url();?>youraccount/createaccount">
+                            <i class="fa fa-lock"></i>
+                            <span class="shop-menu-ttl">INSCRIPTION</span>
+                        </a>
+                        <a href="<?= base_url();?>youraccount/login">
+                            <i class="fa fa-user"></i>
+                            <span class="shop-menu-ttl">CONNEXION</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <div class="h-cart">
+                            <a href="<?= base_url();?>cart">
+                                <i class="fa fa-shopping-cart"></i>
+                                <span class="shop-menu-ttl">MON PANIER</span>
+                                
+                            </a>
+                        </div>
+                    </li>
+
+ 
+
+  <li>
+                        <div class="h-cart">
+                            <a href="<?= base_url() ?>blog/articles">
+                              <i class="fa fa-globe"></i>
+                                <span class="shop-menu-ttl">ACTUAITES ET NOUVEAUTES</span>
+                               
+                            </a>
+                        </div>
+                    </li>
+
+
+ <li>
+                        <div class="h-cart">
+                            <a href="<?php echo base_url()?>chatroom/chatroom.php">
+                              <i class="fa fa-comments-o"></i>
+                                <span class="shop-menu-ttl">NOTRE CHATROOM</span>
+                               
+                            </a>
+                        </div>
+                    </li>
+
+
+ 
+                </ul>
+            </div>
+        </div>
+
+    </div>
